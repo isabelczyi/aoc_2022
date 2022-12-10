2cd2246652bc9f0473c705bf787807b193921b9a
Isabel Cheung <isabelczyi@gmail.com>
2022-12-10 20:42:23 +1000

    day 10 challenge 1

---
+++ day10.js
@@ -0,0 +1,171 @@
+const input = `addx 1
+addx 4
+noop
+noop
+addx 30
+addx -24
+addx -1
+noop
+addx 4
+addx 1
+addx 5
+addx -4
+addx 5
+addx 4
+addx 1
+noop
+addx 5
+addx -1
+addx 5
+addx 3
+noop
+addx -38
+addx 9
+addx -4
+noop
+addx 3
+noop
+addx 2
+addx 3
+noop
+addx 2
+addx 3
+noop
+addx 2
+addx 3
+noop
+addx 2
+addx -17
+addx 22
+addx -2
+addx 5
+addx 2
+addx 3
+addx -2
+addx -36
+noop
+addx 5
+noop
+addx 3
+noop
+addx 2
+addx -5
+noop
+addx 10
+addx 3
+addx -2
+addx 3
+addx 2
+addx 4
+noop
+noop
+noop
+noop
+addx 3
+noop
+noop
+addx 7
+addx 1
+noop
+noop
+addx -38
+addx 39
+addx -32
+noop
+noop
+noop
+addx 5
+addx 2
+addx -1
+addx 4
+noop
+addx 5
+addx -2
+addx 5
+addx 2
+addx -26
+addx 31
+addx -2
+addx 4
+addx 3
+addx -18
+addx 19
+addx -38
+addx 7
+noop
+noop
+addx 34
+addx -39
+addx 8
+addx 5
+addx 2
+addx 10
+addx -5
+addx -2
+addx 5
+addx 2
+addx 11
+addx -6
+noop
+addx 3
+noop
+addx 2
+addx 3
+addx -2
+addx -38
+noop
+noop
+noop
+addx 5
+addx 11
+noop
+addx -3
+noop
+noop
+noop
+addx 2
+noop
+addx -11
+addx 16
+noop
+addx 3
+addx 2
+addx 8
+noop
+noop
+noop
+noop
+noop
+addx 4
+addx 3
+noop
+addx -20
+noop`
+
+let cycle = [1];
+const inputByLine = input.split(/\n/);
+const inputArray = inputByLine.map(line => line.split(' '));
+inputArray.forEach((instruction, index) => {
+  if ((index === 0 && instruction.includes('addx')) ||
+    (inputArray[index - 1].includes('noop') && instruction.includes('addx'))
+  ) {
+    cycle.push(cycle.at(-1), cycle.at(-1), cycle.at(-1) + Number(instruction[1]));
+    return;
+  }
+
+  if (inputArray[index - 1].includes('noop') && instruction.includes('noop')) {
+    cycle.push(cycle.at(-1));
+    return;
+  }
+
+  if (inputArray[index - 1].includes('addx') && instruction.includes('noop')) {
+    return;
+  }
+
+  if (inputArray[index - 1].includes('addx') && instruction.includes('addx')) {
+        cycle.push(cycle.at(-1), cycle.at(-1) + Number(instruction[1]));
+    return;
+  }
+})
+
+console.log((Number(cycle[20]) * 20) +  (Number(cycle[60]) * 60) + (Number(cycle[100]) * 100) + (Number(cycle[140]) * 140) + (Number(cycle[180]) * 180) + (Number(cycle[220]) * 220))
