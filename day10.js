8106b0e0b63d3b1a1b9f7d20fa00e7cda421df4c
Isabel Cheung <isabelczyi@gmail.com>
2022-12-10 21:19:38 +1000

    day 10 challenge 2

--- day10.js
+++ day10.js
@@ -168,4 +168,85 @@ inputArray.forEach((instruction, index) => {
   }
 })
 
-console.log((Number(cycle[20]) * 20) +  (Number(cycle[60]) * 60) + (Number(cycle[100]) * 100) + (Number(cycle[140]) * 140) + (Number(cycle[180]) * 180) + (Number(cycle[220]) * 220))
+// console.log((Number(cycle[20]) * 20) +  (Number(cycle[60]) * 60) + (Number(cycle[100]) * 100) + (Number(cycle[140]) * 140) + (Number(cycle[180]) * 180) + (Number(cycle[220]) * 220))
+
+// part two
+cycle.shift()
+console.log(cycle)
+function sliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        const chunk = arr.slice(i, i + chunkSize);
+        res.push(chunk);
+    }
+    return res;
+}
+const cycleSplit = sliceIntoChunks(cycle, 40);;
+
+const CRTcycleOne = cycleSplit[0].map((position, index) => {
+
+  if (Math.abs(Number(position) - index) < 2) {
+    return '#'
+  } else {
+    return '.'
+  }
+});
+console.log(CRTcycleOne)
+
+const CRTcycleTwo = cycleSplit[1].map((position, index) => {
+
+  if (Math.abs(Number(position) - index) < 2) {
+    return '#'
+  } else {
+    return '.'
+  }
+});
+console.log(CRTcycleTwo)
+
+const CRTcycleThree = cycleSplit[2].map((position, index) => {
+
+  if (Math.abs(Number(position) - index) < 2) {
+    return '#'
+  } else {
+    return '.'
+  }
+});
+console.log(CRTcycleThree)
+
+const CRTcycleFour = cycleSplit[3].map((position, index) => {
+
+  if (Math.abs(Number(position) - index) < 2) {
+    return '#'
+  } else {
+    return '.'
+  }
+});
+console.log(CRTcycleFour)
+
+const CRTcycleFive = cycleSplit[4].map((position, index) => {
+
+  if (Math.abs(Number(position) - index) < 2) {
+    return '#'
+  } else {
+    return '.'
+  }
+});
+console.log(CRTcycleFive)
+
+const CRTcycleSix = cycleSplit[5].map((position, index) => {
+
+  if (Math.abs(Number(position) - index) < 2) {
+    return '#'
+  } else {
+    return '.'
+  }
+});
+console.log(CRTcycleSix)
+
+
+console.log(CRTcycleOne.join())
+console.log(CRTcycleTwo.join())
+console.log(CRTcycleThree.join())
+console.log(CRTcycleFour.join())
+console.log(CRTcycleFive.join())
+console.log(CRTcycleSix.join())
